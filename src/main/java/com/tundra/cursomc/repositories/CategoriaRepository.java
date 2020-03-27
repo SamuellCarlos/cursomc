4aa53eb6ae28822ad6e2003b321474b6e0fc8d3a
SamuellCarlos <samuel.carlos.sc@gmail.com>
2020-03-26 23:05:01 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/tundra/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.tundra.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tundra.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
