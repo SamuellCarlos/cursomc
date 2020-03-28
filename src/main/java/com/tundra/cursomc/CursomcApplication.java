abfdf5ed6748c531712d403503dd24808940c1de
SamuellCarlos <samuel.carlos.sc@gmail.com>
2020-03-28 18:07:58 -0300

    Criando operacao de instanciacao

--- src/main/java/com/tundra/cursomc/CursomcApplication.java
+++ src/main/java/com/tundra/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.tundra.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.tundra.cursomc.domain.Categoria;
+import com.tundra.cursomc.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	public void run(String... args ) {
+			Categoria cat1 = new Categoria(null, "informátia");
+			Categoria cat2 = new Categoria(null, "Escritório");
+			
+			categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		}
+	
 
 }
