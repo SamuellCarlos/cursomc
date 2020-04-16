746f7bb8fee7792902d802a65cdbe66e745d1b11
SamuellCarlos <samuel.carlos.sc@gmail.com>
2020-04-15 23:14:42 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/tundra/cursomc/domain/Test.java
@@ -0,0 +1,16 @@
+package com.tundra.cursomc.domain;
+
+public class Test {
+	private int num;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+
+}
